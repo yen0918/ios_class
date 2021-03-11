a02ac10176607c7382f017420000e3ad8425651d
fuck <C108118137@nkust.edu.tw>
2021-03-11 20:55:18 +0800

    fuck

---
+++ ios_class.swift
@@ -0,0 +1,21 @@
+print("okok")
+let x=4 //宣告常數
+var y=5 //宣告變數
+print(x)
+print(y)
+print(1+1)
+
+var hello:String //明確宣告此變數型別為String
+hello="haha"
+print(hello)
+
+var z:Int=4 //明確宣告此變數型別為int
+print(z)
+
+var e:Double=3.3 //宣告此變數型別為double
+var q=6 as Double //int轉為double才可與double相加
+e=e+3
+//e=e+"33" 數值不能與字串相加
+e=e+q
+e=e-2.0
+print(e - 2)
